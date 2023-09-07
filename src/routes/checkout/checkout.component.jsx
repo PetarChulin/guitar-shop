b3ffbd6aa278a3b7b4fa237bf67db0903995a440
Petar Chulin <petercbg@abv.bg>
2023-09-07 15:46:15 +0300

    modified:   package-lock.json 	modified:   src/components/cart-dropdown/cart-dropdown.styles.scss 	modified:   src/components/confirm-modal/confirm-modal.jsx 	modified:   src/routes/checkout/checkout.component.jsx

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,13 +1,34 @@
 import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
-
+import Button from '../../components/button/button.component';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import Swal from 'sweetalert2';
 
 import './checkout.styles.scss';
 
 const Checkout = () => {
   const { cartItems, cartTotal } = useContext(CartContext);
+
+  const buyAll = () => {
+
+    const cartItemsHtml = `
+    <div style="display: flex; flex-direction: row;">
+      ${cartItems.map(cartItem => `
+        <div style="margin-right: 20px;">
+          <p>${cartItem.name} x ${cartItem.quantity}</p>
+          <img src="${cartItem.imageUrl}" alt="${cartItem.name}" style="max-width: 100px;" />
+          <p>${cartItem.price * cartItem.quantity} $</p>
+        </div>
+      `).join('')}
+    </div>
+  `;
+
+  Swal.fire({
+    html: cartItemsHtml
+  });
+  
+  };
 
   return (
     <div className='checkout-container'>
@@ -32,6 +53,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <span className='total'>Total: ${cartTotal}</span>
+      {cartTotal > 0 && <Button buttonType="inverted" onClick={buyAll}>Buy all</Button>}
     </div>
   );
 };
